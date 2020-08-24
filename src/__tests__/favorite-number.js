a80a1eb1ec875e987cfe3522ae4c90fde57afcdc
Dorell James <galangdj@gmail.com>
2020-08-25 00:25:31 +0800

    Use DOM Testing Library to write more maintanable react tests

--- src/__tests__/favorite-number.js
+++ src/__tests__/favorite-number.js
@@ -1,13 +1,13 @@
 import { FavoriteNumber } from "../favorite-number";
 import React from "react";
 import ReactDOM from "react-dom";
-import { queries } from "@testing-library/dom";
+import { getQueriesForElement } from "@testing-library/dom";
 
 test("it renders input with 'Favorite Number' label", () => {
   const div = document.createElement("div");
   ReactDOM.render(<FavoriteNumber />, div);
+  const { getByLabelText } = getQueriesForElement(div);
 
-  const input = queries.getByLabelText(div, /Favorite Number/i);
-
+  const input = getByLabelText(/Favorite Number/i);
   expect(input).toHaveAttribute("type", "number");
 });
