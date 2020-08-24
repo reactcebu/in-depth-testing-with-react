90f9d8f511acd29dfca216ca89071c73d6073ffd
Dorell James <galangdj@gmail.com>
2020-08-25 00:40:29 +0800

    Test event handler with fireEvent from @testing-library/react

--- src/__tests__/favorite-number.js
+++ src/__tests__/favorite-number.js
@@ -1,9 +1,17 @@
+import { fireEvent, render } from "@testing-library/react";
+
 import { FavoriteNumber } from "../favorite-number";
 import React from "react";
-import { render } from "@testing-library/react";
 
 test("it renders input with 'Favorite Number' label", () => {
   const { getByLabelText } = render(<FavoriteNumber />);
   const input = getByLabelText(/Favorite Number/i);
   expect(input).toHaveAttribute("type", "number");
+});
+
+test("entering an invalid number shows 'The number is invalid'", () => {
+  const { getByLabelText, getByRole } = render(<FavoriteNumber />);
+  const input = getByLabelText(/Favorite Number/i);
+  fireEvent.change(input, { target: { value: 10 } });
+  expect(getByRole("alert")).toHaveTextContent(/The number is invalid/);
 });
