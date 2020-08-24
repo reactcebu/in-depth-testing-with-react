941a8dab161cf2ba20da32db9d78ca9d295a3a3c
Dorell James <galangdj@gmail.com>
2020-08-25 00:21:44 +0800

    Using queries from @testing-library/dom

--- src/__tests__/favorite-number.js
+++ src/__tests__/favorite-number.js
@@ -1,11 +1,13 @@
 import { FavoriteNumber } from "../favorite-number";
 import React from "react";
 import ReactDOM from "react-dom";
+import { queries } from "@testing-library/dom";
 
 test("it renders input with 'Favorite Number' label", () => {
   const div = document.createElement("div");
   ReactDOM.render(<FavoriteNumber />, div);
 
-  expect(div.querySelector("input")).toHaveAttribute("type", "number");
-  expect(div.querySelector("label")).toHaveTextContent("Favorite Number");
+  const input = queries.getByLabelText(div, /Favorite Number/i);
+
+  expect(input).toHaveAttribute("type", "number");
 });
