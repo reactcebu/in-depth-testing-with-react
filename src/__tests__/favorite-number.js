7cd9a6a02a241dea24458375ef87c549ea400916
Dorell James <galangdj@gmail.com>
2020-08-25 00:47:26 +0800

    Use @testing-library/user-event type function instead of fireEvent to improve test confidence

--- src/__tests__/favorite-number.js
+++ src/__tests__/favorite-number.js
@@ -1,7 +1,7 @@
-import { fireEvent, render } from "@testing-library/react";
-
 import { FavoriteNumber } from "../favorite-number";
 import React from "react";
+import { render } from "@testing-library/react";
+import user from "@testing-library/user-event";
 
 test("it renders input with 'Favorite Number' label", () => {
   const { getByLabelText } = render(<FavoriteNumber />);
@@ -12,6 +12,6 @@ test("it renders input with 'Favorite Number' label", () => {
 test("entering an invalid number shows 'The number is invalid'", () => {
   const { getByLabelText, getByRole } = render(<FavoriteNumber />);
   const input = getByLabelText(/Favorite Number/i);
-  fireEvent.change(input, { target: { value: 10 } });
+  user.type(input, "10");
   expect(getByRole("alert")).toHaveTextContent(/The number is invalid/);
 });
