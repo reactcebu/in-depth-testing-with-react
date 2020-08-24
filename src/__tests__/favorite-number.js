a67781e89a1ac596b41d27f40628ca07d53944c0
Dorell James <galangdj@gmail.com>
2020-08-25 00:32:21 +0800

    Swap abstraction with built-in render function from @testing-library/react

--- src/__tests__/favorite-number.js
+++ src/__tests__/favorite-number.js
@@ -1,15 +1,6 @@
 import { FavoriteNumber } from "../favorite-number";
 import React from "react";
-import ReactDOM from "react-dom";
-import { getQueriesForElement } from "@testing-library/dom";
-
-function render(ui) {
-  const container = document.createElement("div");
-  ReactDOM.render(ui, container);
-  const queries = getQueriesForElement(container);
-
-  return { container, ...queries };
-}
+import { render } from "@testing-library/react";
 
 test("it renders input with 'Favorite Number' label", () => {
   const { getByLabelText } = render(<FavoriteNumber />);
