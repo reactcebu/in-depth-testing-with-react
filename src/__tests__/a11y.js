bad939e2552c987416cf96fc4c4fd74a4bc03ef2
Dorell James <galangdj@gmail.com>
2020-08-25 23:55:02 +0800

    Better violations result using toHaveNoValidations

--- src/__tests__/a11y.js
+++ src/__tests__/a11y.js
@@ -1,12 +1,14 @@
+import { axe, toHaveNoViolations } from "jest-axe";
+
 import React from "react";
-import { axe } from "jest-axe";
 import { render } from "@testing-library/react";
 
 const { InaccessibleForm } = require("../a11y");
+expect.extend(toHaveNoViolations);
 
 test("the form is accessible", async () => {
   const { container } = render(<InaccessibleForm />);
   console.log(container.innerHTML);
   const results = await axe(container);
-  expect(results.violations).toHaveLength(0);
+  expect(results).toHaveNoViolations();
 });
