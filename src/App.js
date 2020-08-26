f5347217fd5560223e9e2e69e5eb25d2093924ea
Dorell James <galangdj@gmail.com>
2020-08-26 16:56:45 +0800

    Add GreetingLoader component and API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { GreetingLoader } from "./greeting-loader";
 import React from "react";
 
 function App() {
-  return <div className="App">Start writing tests at __tests__ directory.</div>;
+  return (
+    <div className="App">
+      Start writing tests at __tests__ directory. <GreetingLoader />
+    </div>
+  );
 }
 
 export default App;
