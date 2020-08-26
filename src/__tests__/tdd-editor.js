15d3bec1d9e7bdd41ce883db07eaf2870ffe0ea5
Dorell James <galangdj@gmail.com>
2020-08-27 01:00:19 +0800

    Update test data with close to real life data

--- src/__tests__/tdd-editor.js
+++ src/__tests__/tdd-editor.js
@@ -1,3 +1,4 @@
+import { build, fake } from "test-data-bot";
 import { fireEvent, render } from "@testing-library/react";
 
 import { Editor } from "../editor";
@@ -6,15 +7,17 @@ import { savePost as mockSavePost } from "../api";
 
 jest.mock("../api");
 
+const postBuilder = build("Post").fields({
+  title: fake((f) => f.lorem.words()),
+  content: fake((f) => f.lorem.paragraph()),
+  tags: fake((f) => [f.lorem.words(), f.lorem.words(), f.lorem.words()]),
+});
+
 test("it renders a form with title, content, tags, and a submit button", () => {
   const { getByLabelText, getByText } = render(<Editor />);
   mockSavePost.mockResolvedValueOnce();
 
-  const fakePost = {
-    title: "New Title",
-    content: "Test content",
-    tags: ["tags1", "tags2"],
-  };
+  const fakePost = postBuilder();
 
   getByLabelText(/title/i).value = fakePost.title;
   getByLabelText(/content/i).value = fakePost.content;
