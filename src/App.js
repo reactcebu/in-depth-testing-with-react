5113433c2a06daec92fb9c5de7d1db0cc298cf13
Dorell James <galangdj@gmail.com>
2020-08-26 20:48:42 +0800

    Simulate our ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { ErrorBoundary } from "./error-boundary";
 import React from "react";
 
+function Bomb({ shouldThrow }) {
+  if (shouldThrow) {
+    throw new Error("💣");
+  } else {
+    return null;
+  }
+}
+
 function App() {
-  return <div className="App">Start writing tests at __tests__ directory.</div>;
+  return (
+    <div className="App">
+      Start writing tests at __tests__ directory.
+      <ErrorBoundary>
+        <Bomb shouldThrow={true} />
+      </ErrorBoundary>
+    </div>
+  );
 }
 
 export default App;
