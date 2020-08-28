859081dd6ed4b5b97809ee30ca2baf5ff15702b9
Dorell James <galangdj@gmail.com>
2020-08-28 15:33:08 +0800

    Initial components setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Fetch from "./fetch";
 
 function App() {
-  return <div className="App">Start writing tests at __tests__ directory.</div>;
+  return (
+    <div className="App">
+      <Fetch url="https://wt-873f548fdd5b60d59d25e0cae4a5051c-0.sandbox.auth0-extend.com/load-greeting" />
+    </div>
+  );
 }
 
 export default App;
