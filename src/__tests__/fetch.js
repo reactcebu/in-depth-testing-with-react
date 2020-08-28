ceff4ed9c3b1e50405dec29a73edd170dcc9e11d
Dorell James <galangdj@gmail.com>
2020-08-28 16:43:53 +0800

    Mocking with msw

--- src/__tests__/fetch.js
+++ src/__tests__/fetch.js
@@ -1,17 +1,25 @@
 import React from "react";
 
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
 import { render } from "@testing-library/react";
 import { Fetch } from "../fetch";
 import userEvent from "@testing-library/user-event";
 
-import axios from "axios";
+const server = setupServer(
+  rest.get("/greeting", (req, res, ctx) => {
+    return res(ctx.json({ greeting: "Hello!" }));
+  })
+);
 
-jest.mock("axios");
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 it("shows greeting fetched from API", async () => {
   const greeting = "Hello!";
   const url = "/greeting";
-  axios.get.mockResolvedValueOnce({ data: { greeting } });
 
   const { getByRole, findByRole } = render(<Fetch url={url} />);
 
@@ -19,6 +27,20 @@ it("shows greeting fetched from API", async () => {
   userEvent.click(loadGreetingBtn);
 
   expect(await findByRole("heading")).toHaveTextContent(greeting);
-  expect(axios.get).toHaveBeenCalledTimes(1);
-  expect(axios.get).toHaveBeenCalledWith(url);
+});
+
+test("handles server error", async () => {
+  server.use(
+    rest.get(`/greeting`, (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
+  );
+  const url = "/greeting";
+
+  const { getByRole, findByRole } = render(<Fetch url={url} />);
+
+  const loadGreetingBtn = getByRole("button");
+  userEvent.click(loadGreetingBtn);
+
+  expect(await findByRole("alert")).toHaveTextContent("Oops, failed to fetch!");
 });
