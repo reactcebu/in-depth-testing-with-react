a70ea5713d442cfc023b7dca018ef8704cdc5837
Dorell James <galangdj@gmail.com>
2020-08-28 09:35:54 +0800

    Rendering our Counter component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { store } from "./redux-store";
+import { Provider } from "react-redux";
+import { Counter } from "./redux-counter";
 
 function App() {
-  return <div className="App">Start writing tests at __tests__ directory.</div>;
+  return (
+    <div className="App">
+      Start writing tests at __tests__ directory.
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    </div>
+  );
 }
 
 export default App;
