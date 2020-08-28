5a6c71c225720b985554d1775b7599d97cfcf730
Dorell James <galangdj@gmail.com>
2020-08-28 09:53:48 +0800

    Refactor our render function to simplify test

--- src/__tests__/redux-counter.js
+++ src/__tests__/redux-counter.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { Counter } from "../redux-counter";
-import { render } from "@testing-library/react";
+import { render as rtlRender } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { store as appStore } from "../redux-store";
 import { createStore } from "redux";
 import { reducer } from "../redux-reducer";
 
+function render(
+  ui,
+  { initialState, store = createStore(reducer, initialState), ...options } = {}
+) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return rtlRender(ui, { wrapper: Wrapper, ...options });
+}
+
 test("it can render Counter with redux defaults", () => {
-  const { getByText, getByLabelText } = render(
-    <Provider store={appStore}>
-      <Counter />
-    </Provider>
-  );
+  const { getByText, getByLabelText } = render(<Counter />);
 
   const plusBtn = getByText("+");
   userEvent.click(plusBtn);
@@ -22,12 +28,9 @@ test("it can render Counter with redux defaults", () => {
 });
 
 test("it can render Counter with redux initialized state", () => {
-  const store = createStore(reducer, { count: 3 });
-  const { getByText, getByLabelText } = render(
-    <Provider store={store}>
-      <Counter />
-    </Provider>
-  );
+  const { getByText, getByLabelText } = render(<Counter />, {
+    initialState: { count: 3 },
+  });
 
   const plusBtn = getByText("-");
   userEvent.click(plusBtn);
