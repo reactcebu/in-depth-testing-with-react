b477f80c99edbc5d77db0a8bc7b01328e4734a19
Dorell James <galangdj@gmail.com>
2020-08-28 09:35:40 +0800

    Test with initial state

--- src/__tests__/redux-counter.js
+++ src/__tests__/redux-counter.js
@@ -3,11 +3,13 @@ import { Provider } from "react-redux";
 import { Counter } from "../redux-counter";
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { store } from "../redux-store";
+import { store as appStore } from "../redux-store";
+import { createStore } from "redux";
+import { reducer } from "../redux-reducer";
 
 test("it can render Counter with redux defaults", () => {
   const { getByText, getByLabelText } = render(
-    <Provider store={store}>
+    <Provider store={appStore}>
       <Counter />
     </Provider>
   );
@@ -17,4 +19,19 @@ test("it can render Counter with redux defaults", () => {
 
   const countLabel = getByLabelText(/count/i);
   expect(countLabel).toHaveTextContent("1");
+});
+
+test("it can render Counter with redux initialized state", () => {
+  const store = createStore(reducer, { count: 3 });
+  const { getByText, getByLabelText } = render(
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+
+  const plusBtn = getByText("-");
+  userEvent.click(plusBtn);
+
+  const countLabel = getByLabelText(/count/i);
+  expect(countLabel).toHaveTextContent("2");
 });
